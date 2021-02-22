56b5b885461a3b2e6a1dcdc589b8e855acbe8654
EIndriksons <edgars@indriksons.lv>
2021-02-22 12:28:29 +0200

    Lecture 1 - Puzzle knight solution

--- 1-knowledge/project1/knights/puzzle.py
+++ 1-knowledge/project1/knights/puzzle.py
@@ -12,21 +12,48 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # Implement rules of game
+    # It is either Knight or Knave (OR)
+    Or(AKnight, AKnave),
+    # If it is Knave then it is not Knight and vice versa
+    Implication(AKnight, Not(AKnave)),
+    Implication(AKnave, Not(AKnight)),
+
+    # Consider what A said
+    Biconditional(AKnight, And(AKnight, AKnave)) # If I am a Knight then I am both Knight and Knave (False, I cannot be both)
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # Again implement rules of game but with two pairs
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Implication(AKnight, Not(AKnave)),
+    Implication(AKnave, Not(AKnight)),
+    Implication(BKnight, Not(BKnave)),
+    Implication(BKnave, Not(BKnight)),
+
+    # Consider what A said
+    Biconditional(AKnight, And(AKnave, BKnave)) # If I am a Knight then me (A) and B are Knaves (False, because Knight cant be Knave)
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # Rules of game
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Implication(AKnight, Not(AKnave)),
+    Implication(AKnave, Not(AKnight)),
+    Implication(BKnight, Not(BKnave)),
+    Implication(BKnave, Not(BKnight)),
+
+    # Consider what A and B said
+    Biconditional(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))), # If I am a Knight then either we are both Knights or both Knaves (False, contradicts next sentence)
+    Biconditional(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))) # If I am a Knight then either one of us is knight or knave, or other is knave or knight
 )
 
 # Puzzle 3
@@ -35,7 +62,22 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    # Rules of game
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    Implication(AKnight, Not(AKnave)),
+    Implication(AKnave, Not(AKnight)),
+    Implication(BKnight, Not(BKnave)),
+    Implication(BKnave, Not(BKnight)),
+    Implication(CKnight, Not(CKnave)),
+    Implication(CKnave, Not(CKnight)),
+
+    # Consider what A, B and C said
+    Biconditional(AKnight, Or(AKnight, AKnave)), # If I am a Knight then I am either Knights or Knave
+    Biconditional(BKnight, Biconditional(AKnight, AKnave)), # If I am Knight then A is a Knave
+    Biconditional(BKnight, CKnave), # If I am a Knight then C is a Knave
+    Biconditional(CKnight, AKnight) # If I am a Knight then A is a Knight
 )
 
 
